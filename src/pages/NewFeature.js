760759b20cb07bae560e22e3e816754ec8603877
Fakhri <fakhridho95@gmail.com>
2021-04-26 13:48:54 +0700

    update

--- src/pages/NewFeature.js
+++ src/pages/NewFeature.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function NewFeature() {
   return (
     <div>
-      <h1>Welcome to NewFeature</h1>
+      <h1>Welcome to NewFeature Yeah</h1>
     </div>
   );
 }
