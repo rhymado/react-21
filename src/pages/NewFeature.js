a4559e32498aab284e3f6eebaabd24a688d90c1a
Fakhri <fakhridho95@gmail.com>
2021-04-26 14:01:00 +0700

    new function added

--- src/pages/NewFeature.js
+++ src/pages/NewFeature.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 export default function NewFeature() {
+  const callFunction = (a, b) => {
+    return { a, b };
+  };
   return (
     <div>
       <h1>Welcome to NewFeature Yeah</h1>
+      <p>{JSON.stringify(callFunction(1, 2))}</p>
     </div>
   );
 }
